98b907556bc00979876bff719ef22034b45e8271
ariars <ariars@outlook.com>
2024-03-02 00:55:32 +0900

    ch03_02 완료

---
+++ ch03/ch03_2/src/App.tsx
@@ -0,0 +1,14 @@
+import Color from './pages/Color'
+import Tailwindcss from './pages/Tailwindcss'
+import TextsTest from './pages/TextsTest'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <Color />
+      <TextsTest />
+      <Tailwindcss />
+    </div>
+  )
+}
