f8efe9bb76dfa687c119ceaec24a925187fefa01
ariars <ariars@outlook.com>
2024-03-02 21:32:32 +0900

    ch03_3 완료

---
+++ ch03/ch03_3/src/pages/BorderTest.tsx
@@ -0,0 +1,9 @@
+import { Title } from "../components";
+export default function BorderTest() {
+  return (
+    <section className="mt-4">
+      <Title>BorderTest</Title>
+      <div className="mt-4"></div>
+    </section>
+  );
+}
