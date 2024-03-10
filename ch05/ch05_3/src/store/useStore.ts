06fdcd18e4ce825f9a35edbfbfbc80e8fa184533
ariars <ariars@outlook.com>
2024-03-10 12:17:09 +0900

    ch05_3 완료

---
+++ ch05/ch05_3/src/store/useStore.ts
@@ -0,0 +1,26 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { useMemo } from 'react'
+import { rootReducer } from './rootReducer'
+//import logger from './logger'
+import logger from 'redux-logger'
+import { thunk } from 'redux-thunk'
+
+const useLogger = process.env.NODE_ENV !== 'production'
+
+const initializeStore = () => {
+  const middlewares: any[] = [thunk]
+  if (useLogger) {
+    middlewares.push(logger)
+  }
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(middlewares)
+  })
+
+  return store
+}
+
+export function useStore() {
+  const store = useMemo(() => initializeStore(), [])
+  return store
+}
